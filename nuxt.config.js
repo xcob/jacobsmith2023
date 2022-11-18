0d15cfdaa8f0720920634e0dc40d0b643207edd0
cob <34405172+xcob@users.noreply.github.com>
2022-11-17 23:54:21 -0600

    page title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'jacobsmith2023',
+    title: 'Jacob Smith | Software Engineer - Digital Marketer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
